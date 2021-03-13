d6926361c934412b525abc1637202ec6b7ff151a
FP_kl <pang.jc@qq.com>
2021-03-13 14:53:59 +0800

    set block layout to Nx2

--- block_gen.m
+++ block_gen.m
@@ -8,7 +8,7 @@ blockStruct = jsondecode(blockTxt);
 
 numOfBlocks = length(blockStruct.Blocks);
 
-fprintf('%d blocks found in config file.\n',numOfBlocks)
+tic;
 
 for j = 1:numOfBlocks
     subName = blockStruct.Blocks(j).Name;
@@ -55,6 +55,24 @@ for j = 1:numOfBlocks
     display_code = append(display_code, display_name);
 
     thisMask.set('Display', display_code)
+
+    % calculate block position
+    bwidth = 140;
+    bxSpacing = 50;
+    bhight = 52;
+    bySpacing = 17;
+    if rem(j,2)==0
+        xpos = [bwidth+bxSpacing bwidth*2+bxSpacing];
+    else
+        xpos = [0 bwidth];
+    end
+    ypos = [(bhight+bySpacing)*(ceil(j/2)-1) (bhight+bySpacing)*(ceil(j/2)-1)+bhight];
+
+    % set block position
+    set_param(subPath,'position',[xpos(1) ypos(1) xpos(2) ypos(2)]);
 end
 
 save_system(md)
+
+timecost = toc;
+fprintf('Generate %d block(s) successfully in %f s.\nCheck the output in %s.slx.\n', numOfBlocks, timecost, mdName)
